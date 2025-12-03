d8085e5a7d214de3413dc31491a4a2e6e44fa82d
flynnlee <lingfei@lynxaura.com>
2025-12-03 12:01:24 +0800

    fix: add Radius

--- src/master.ts
+++ src/master.ts
@@ -30,7 +30,7 @@ export function startMaster(native: any) {
   
   // 核心优化：防抖动与幽灵事件过滤
   let isResetting = false; // 是否正在等待瞬移完成
-  const SAFE_RADIUS = 300; // 安全半径，鼠标在这个范围内不瞬移，保证丝滑
+  const SAFE_RADIUS = 500; // 安全半径，鼠标在这个范围内不瞬移，保证丝滑
 
   const connect = () => {
     socket = new net.Socket();
@@ -71,7 +71,7 @@ export function startMaster(native: any) {
   uIOhook.on("input", (e: any) => {
     if (!isRemote || !socket) return;
 
-    // 1. 鼠标移动 (核心优化逻辑)
+    // 1. 鼠标移动 (核心优化逻辑)Y
     if (e.type === EVENT_TYPE.MOUSE_MOVED) {
       // 如果处于“重置中”状态，我们需要过滤掉那些“瞬移前”的残留事件
       // 只有当接收到的坐标非常接近中心点时，才认为瞬移完成了
